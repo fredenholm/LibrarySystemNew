694d18211ff363d7eaae1532c543b8405a42cc12
david <Davidcla@hotmail.com>
2014-10-21 10:35:56 +0200

    så

--- LibrarySystem/Login.aspx.designer.cs
+++ LibrarySystem/Login.aspx.designer.cs
@@ -47,14 +47,5 @@ namespace LibrarySystem {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button loginBtn;
-        
-        /// <summary>
-        /// createBtn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button createBtn;
     }
 }
